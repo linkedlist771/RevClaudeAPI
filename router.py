fae868adb197c811ed434888e101a26a2002951c
LiDing <72634327+linkedlist771@users.noreply.github.com>
2024-04-21 14:54:49 +0800

    Feature: add apikey authenticator

--- router.py
+++ router.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, HTTPException, Request, APIRouter
 
 from claude_router import router as claude_router
-
+from api_key_router import router as api_key_router
 
 router = APIRouter(prefix="/api/v1")
 router.include_router(claude_router, prefix="/claude")
+router.include_router(api_key_router, prefix="/api_key")
