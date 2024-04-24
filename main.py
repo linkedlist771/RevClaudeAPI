dd7b0bc9c89f782662ed612feb507e3e59a5c67d
LiDing <72634327+linkedlist771@users.noreply.github.com>
2024-04-24 22:03:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -123,8 +123,8 @@ async def index():
 
 
 @app.get("/api/v1/client_status")
-async def get_client_status():
-    return get_cookie_manager(basic_clients, plus_clients)
+async def _get_client_status():
+    return get_client_status(basic_clients, plus_clients)
 
 
 def start_server(port=args.port, host=args.host):
