02c16830f107e66195bf9e589bb89aa581df3edb
LiDing <72634327+linkedlist771@users.noreply.github.com>
2024-05-05 23:23:31 +0800

    Update claude.py

--- claude.py
+++ claude.py
@@ -271,6 +271,7 @@ class Client:
                 "prompt": f"{prompt}",
             }
         )
+        logger.debug(f"payload: {payload}")
 
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/124.0",
