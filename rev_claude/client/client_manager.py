5fb3d06b15e9a34ef0a77c4a00e0b8e10a43d0ef
LiDing <72634327+linkedlist771@users.noreply.github.com>
2024-06-05 01:00:03 +0800

    Update client_manager.py

--- rev_claude/client/client_manager.py
+++ rev_claude/client/client_manager.py
@@ -1,5 +1,6 @@
 from rev_claude.cookie.claude_cookie_manage import get_cookie_manager
 
+HASH_MODULE = 1e6
 
 class ClientManager:
     basic_clients:  dict = {}
@@ -11,10 +12,10 @@ class ClientManager:
         cookie_manager = get_cookie_manager()
         basic_clients, plus_clients = cookie_manager.get_all_basic_and_plus_client()
         ClientManager.basic_clients = {
-            hash(client.cookie_key): client for client in basic_clients
+            hash(client.cookie_key) % HASH_MODULE: client for client in basic_clients
         }
         ClientManager.plus_clients = {
-            hash(client.cookie_key): client for client in plus_clients
+            hash(client.cookie_key) % HASH_MODULE: client for client in plus_clients
         }
 
     def get_clients(self):
