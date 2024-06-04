73e3d2aa5d8e0abe30457bbc4b55688b11a2685e
LiDing <72634327+linkedlist771@users.noreply.github.com>
2024-06-05 01:28:52 +0800

    Update client_manager.py

--- rev_claude/client/client_manager.py
+++ rev_claude/client/client_manager.py
@@ -3,6 +3,10 @@ from loguru import logger
 HASH_MODULE = 1e6
 
 
+def custom_hash(key: str):
+    # 获取每个char的ascii码，然后相加
+    return sum([ord(c) for c in key])
+
 class ClientManager:
     basic_clients: dict = {}
     plus_clients: dict = {}
@@ -11,10 +15,10 @@ class ClientManager:
         cookie_manager = get_cookie_manager()
         basic_clients, plus_clients = cookie_manager.get_all_basic_and_plus_client()
         ClientManager.basic_clients = {
-            int(hash(client.cookie_key) % HASH_MODULE): client for client in basic_clients
+            int(custom_hash(client.cookie_key) % HASH_MODULE): client for client in basic_clients
         }
         ClientManager.plus_clients = {
-            int(hash(client.cookie_key) % HASH_MODULE): client for client in plus_clients
+            int(custom_hash(client.cookie_key) % HASH_MODULE): client for client in plus_clients
         }
         logger.info(f"basic_clients: {ClientManager.basic_clients.keys()}")
         logger.info(f"plus_clients: {ClientManager.plus_clients.keys()}")
