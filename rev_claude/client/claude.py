03028384e43820667aefbaed6067a9e2fdc42f16
LiDing <72634327+linkedlist771@users.noreply.github.com>
2024-06-20 15:19:02 +0800

    Update claude.py

--- rev_claude/client/claude.py
+++ rev_claude/client/claude.py
@@ -316,7 +316,9 @@ class Client:
                                              proxies=PROXIES if USE_PROXY else None
 
                                              ) as client:
-                 async with client.stream(method="POST", url=url, headers=headers, json=payload) as response:
+                 async with client.stream(method="POST", url=url, headers=headers, json=payload,
+                                          timeout=STREAM_TIMEOUT
+                                          ) as response:
                   async for text in response.aiter_lines():
                     # logger.debug(f"raw text: {text}")
                     # async with client.stream(method="POST", url=url, headers=headers, json=data) as response:
