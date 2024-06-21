a6dd4b2b5e36f7b67dfe061dfd54b48d0263e054
LiDing <72634327+linkedlist771@users.noreply.github.com>
2024-06-21 12:46:30 +0800

    Update models.py

--- rev_claude/models.py
+++ rev_claude/models.py
@@ -5,10 +5,11 @@ class ClaudeModels(Enum):
     SONNET = "claude-3-sonnet-20240229"
     HAIKU = "claude-3-haiku-20240307"
     OPUS = "claude-3-opus-20240229"
+    SONNET_3_5 = "claude-3-5-sonnet-20240620"
 
     @staticmethod
     def model_is_plus(model):
-        return model in [ClaudeModels.OPUS.value, ClaudeModels.HAIKU.value]
+        return model in [ClaudeModels.OPUS.value, ClaudeModels.HAIKU.value, ClaudeModels.SONNET_3_5.value]
 
     @staticmethod
     def model_is_basic(model):
