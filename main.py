c53f38c10c536e6192fb70b1880705f73af26212
LiDing <72634327+linkedlist771@users.noreply.github.com>
2024-06-26 01:15:57 +0800

    Rever changes

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import argparse
 import fire
 import uvicorn
-from fastapi import FastAPI, Query, HTTPException
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from loguru import logger
 
-
-from rev_claude.api_key.api_key_manage import get_api_key_manager
 from rev_claude.client.client_manager import ClientManager
 from rev_claude.lifespan import lifespan
 from rev_claude.middlewares.register_middlewares import register_middleware
@@ -25,13 +23,7 @@ app = register_middleware(app)
 
 
 @app.get("/api/v1/clients_status")
-async def _get_client_status(api_key: str = Query(None, alias="apikey")):
-    manager = get_api_key_manager()
-    logger.debug(f"API Key:{api_key}")
-    if not api_key:
-        raise HTTPException(status_code=401, detail="API Key is required")
-    if not manager.is_api_key_valid(api_key):
-        raise HTTPException(status_code=401, detail="Invalid API Key")
+async def _get_client_status():
     basic_clients, plus_clients = ClientManager().get_clients()
     return get_client_status(basic_clients, plus_clients)
 
