ea30a416583eb89a4e23cd413763f4674ac8b952
LiDing <72634327+linkedlist771@users.noreply.github.com>
2024-07-01 16:09:00 +0800

    Update claude.py

--- rev_claude/client/claude.py
+++ rev_claude/client/claude.py
@@ -291,6 +291,8 @@ class Client:
                 async with httpx.AsyncClient(
                     timeout=STREAM_TIMEOUT, proxies=PROXIES if USE_PROXY else None
                 ) as client:
+                    logger.debug(f"url:\n {url}")
+                    logger.debug(f"headers:\n {headers}")
                     async with client.stream(
                         method="POST",
                         url=url,
