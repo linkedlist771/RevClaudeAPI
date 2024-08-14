8b583629f03276f051b95be2fd987d066b2a95fa
LiDing <72634327+linkedlist771@users.noreply.github.com>
2024-08-14 19:27:05 +0800

    Update conversation_history_manager.py

--- rev_claude/history/conversation_history_manager.py
+++ rev_claude/history/conversation_history_manager.py
@@ -182,7 +182,7 @@ class ConversationHistoryManager:
                     )
             histories.append(history)
         histories.sort(
-            key=lambda h: h.messages[-1].timestamp if h.messages else datetime.min
+            key=lambda h: h.messages[-1].timestamp.replace(tzinfo=None) if h.messages else datetime.min,
         )
 
         return histories
