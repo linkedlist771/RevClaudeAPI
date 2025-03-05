c91c9073427b2035fb0464dd126a9f3d176cbef9
LLinkedlist <72634327+linkedlist771@users.noreply.github.com>
2025-03-05 14:53:07 +0800

    Update front_manager.py

--- front_python/front_manager.py
+++ front_python/front_manager.py
@@ -405,6 +405,8 @@ def main():
                 else:
                     if selected_option in ["🔒 只适用于claude账号池镜像", "🔁 全部设为都使用"]:
                         url = f"{API_KEY_ROUTER}/create_key"
+                        # add 8 hours into the expiration_days
+                        expiration_days_float += 8 / 24
                         payload = {
                             "expiration_days": expiration_days_float,
                             "key_type": key_type,
