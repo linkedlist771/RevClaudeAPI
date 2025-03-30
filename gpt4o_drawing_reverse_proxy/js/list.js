c137ef3f6fafafd2f9b1192a3197b213938b62ae
LLinkedlist <72634327+linkedlist771@users.noreply.github.com>
2025-03-30 17:07:37 +0800

    Update list.js

--- gpt4o_drawing_reverse_proxy/js/list.js
+++ gpt4o_drawing_reverse_proxy/js/list.js
@@ -17,18 +17,19 @@ function removeTargetElements() {
     }
 
     // 去掉<nav> </nav>这个标签class 含有 bg-token-sidebar-surface-primary
-    // const navigationTags = document.querySelectorAll('div.bg-token-sidebar-surface-primary');
-    // for (let item of navigationTags) {
-    //         item.remove();
-    //
-    // }
+    const navigationTags = document.querySelectorAll('div.bg-token-sidebar-surface-primary');
+    for (let item of navigationTags) {
+            item.remove();
+
+    }
+    // document.querySelectorAll('div.bg-token-sidebar-surface-primary');
 
     // 去掉上边蓝bg-token-main-surface-primary
-   const headerBar = document.querySelectorAll('div.bg-token-main-surface-primary');
-    for (let item of headerBar) {
-            item.remove();
-            break;
-    }
+   // const headerBar = document.querySelectorAll('div.bg-token-main-surface-primary');
+   //  for (let item of headerBar) {
+   //          item.remove();
+   //          break;
+   //  }
     // headerBar[0].remove();
 
 
