69fce447e0a6e12241210493974207dc65d2b889
LLinkedlist <72634327+linkedlist771@users.noreply.github.com>
2025-04-01 13:39:25 +0800

    Update server.py

--- gpt4o_drawing_reverse_proxy/server.py
+++ gpt4o_drawing_reverse_proxy/server.py
@@ -68,7 +68,7 @@ def yulan_js():
     js_content = read_js_file('yulan.js')
     return Response(js_content, mimetype='application/javascript')
 
-@app.route('/editPassword', methods=['POST'])
+@app.route('/editPassword')
 def edit_password():
     gpt_manager = get_souruxgpt_manager()
 
